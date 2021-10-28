2c6ad5881493b3ecae1dd346c96525026a5f4c37
alvaro <53096383+alvarosanchezduran@users.noreply.github.com>
2021-10-28 23:41:13 +0200

    Adding toolbar

--- app/src/main/java/com/android/f1/results/MainActivity.kt
+++ app/src/main/java/com/android/f1/results/MainActivity.kt
@@ -3,6 +3,8 @@ package com.android.f1.results
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.android.f1.results.databinding.MainActivityBinding
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
@@ -11,9 +13,19 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
+    private lateinit var binding: MainActivityBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setUpDatabinding()
+    }
+
+    private fun setUpDatabinding() {
+        binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+                .apply {
+                    lifecycleOwner = this@MainActivity
+                }
     }
 
     override fun supportFragmentInjector() = dispatchingAndroidInjector
